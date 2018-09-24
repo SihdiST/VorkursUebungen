760a9b5686af7b140c7aec0590951f13ec9607f3
SihdiST <sihdi.st@gmail.com>
2018-09-24 18:15:49 +0000

    Update buch.h

--- buch.h
+++ buch.h
@@ -1,8 +1,12 @@
+
 typedef struct{
 	char isbn[100];
 	char name[100];
 	char author[100];
 }Buch;
 
-void editBook(Buch* pBook, char[] pISBN, char[] pName, char[] pAuthor);
-Buch createBook(char[] pISBN, char[] pName, char[] pAuthor);
+//#define NULLBUCH Buch nullbuch; nullbuch.isbn[0] = nullbuch.name[0] = nullbuch.author[0] = 'N'; nullbuch.isbn[1] = nullbuch.name[1] = nullbuch.author[1] ='U'; nullbuch.isbn[2] = nullbuch.name[2] = nullbuch.author[2] ='L';nullbuch.isbn[3] = nullbuch.name[3] = nullbuch.author[3] = 'L';
+
+void editBook(Buch* pBook, char pISBN[], char pName[], char pAuthor[]);
+Buch createBook(char pISBN[], char pName[], char pAuthor[]);
+int cmpBooks(Buch pCmp1, Buch pCmp2);
