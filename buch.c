d103e6bfb92bae4e78305cc56a9852d04bd24dda
SihdiST <sihdi.st@gmail.com>
2018-09-24 11:41:24 +0000

    Update buch.c

--- buch.c
+++ buch.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
 #include "buch.h"
+
+void editBook(Buch* pBook, char[] pISBN, char[] pName, char[] pAuthor){
+	Book edit = createBook(pISBN, pName, pAuthor);
+	*pBook = edit;
+}
+
+Buch createBook(char[] pISBN, char[] pName, char[] pAuthor){
+	Buch new;
+	Buch.isbn = pISBN;
+	Buch.name = pName;
+	Buch.author = pAuthor;
+	return new;
+}
+
+void print(Buch){
+
+}
+
+int main(void){
+	return 0;
+}
