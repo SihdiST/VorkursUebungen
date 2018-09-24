46f16b631a42cbd2c2abc7c63a761fe792e8b097
dorianheidorn <43538605+dorianheidorn@users.noreply.github.com>
2018-09-24 12:33:25 +0000

    Update kunde.c

--- kunde.c
+++ kunde.c
@@ -2,12 +2,12 @@
 #include <string.h>
 #include "kunde.h"
 
-void editKunde(Kunde* pKunde, int pNummer, String pName, String pVorname){
-	Kunde edit = createKunde(int pNummer, String pName, String pVorname);
+void editKunde(Kunde* pKunde, int pNummer, char *pName, char *pVorname){
+	Kunde edit = createKunde( pNummer, pName, pVorname);
 	*pKunde = edit;
 }
 
-Kunde createKunde(int pNummer, String pName, String pVorname){
+Kunde createKunde(int pNummer, char *pName, char *pVorname){
 	Kunde new;
 	new.kundenNr = pNummer;
 	strcpy(new.name, pName);
