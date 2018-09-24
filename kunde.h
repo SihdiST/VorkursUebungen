782c35eb6c879bebd9c27818d386d59d75d7054a
dorianheidorn <43538605+dorianheidorn@users.noreply.github.com>
2018-09-24 12:33:48 +0000

    Update kunde.h

--- kunde.h
+++ kunde.h
@@ -1,8 +1,9 @@
 typedef struct kunde{
 	int kundenNr;
-	char[100] = name;
-	char[100] = vorname;
+	char name [100];
+	char vorname [100];
 }Kunde;
-void editKunde(Kunde pKunde);
 
-Kunde createKunde(int pNummer, String pName, String pVorname);
+//void editKunde(Kunde pKunde);
+
+Kunde createKunde(int pNummer, char *pName, char *pVorname);
