e59d152677ad3e34515b1202448dffa8c52b3660
SihdiST <sihdi.st@gmail.com>
2018-09-24 20:06:55 +0000

    Update main.c
    fixed

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include "buch.h"
 #include "kunde.h"
 
-Buch nullbuch;
 
 int searchBook(char pSearch[], int pSort, Buch pBooks[], int pLength){
 	if(pSort == 0){
@@ -37,7 +36,7 @@ void addBook(char pISBN[], char pName[], char pAuthor[], Buch pBooks[], int pLen
 	if((searchBook(pISBN, 0, pBooks, pLength) != 0) && (searchBook(pName, 1, pBooks, pLength) != 0) &&  (searchBook(pAuthor, 2, pBooks, pLength) != 0)) return;
 	Buch add = createBook(pISBN, pName, pAuthor);
 	 
-	//nullbuch //def in buch.h
+	NULLBUCH //def in buch.h
 	
 	int i = 0;
 	while(i < pLength){
@@ -53,7 +52,7 @@ void addBook(char pISBN[], char pName[], char pAuthor[], Buch pBooks[], int pLen
 
 void deleteBook(char pSearch[], int pSort, Buch pBooks[], int pLength){
 	
-	//nullbuch
+	NULLBUCH
 	
 	if(searchBook(pSearch, pSort, pBooks, pLength) != 0){
 		int toDelete = (searchBook(pSearch, pSort, pBooks, pLength)) - 1;	
@@ -64,21 +63,6 @@ void deleteBook(char pSearch[], int pSort, Buch pBooks[], int pLength){
 
 int main(void){
 	
-	
-	nullbuch.isbn[0] = 'N';
-	nullbuch.isbn[1] = 'U';
-	nullbuch.isbn[2] = 'L';
-	nullbuch.isbn[3] = 'L';
-	
-	nullbuch.name[0] = 'N';
-	nullbuch.name[1] = 'U';
-	nullbuch.name[2] = 'L'; 
-	nullbuch.name[3] = 'L';
-	
-	nullbuch.author[0] = 'N';
-	nullbuch.author[1] = 'U';
-	nullbuch.author[2] = 'L';
-	nullbuch.author[3] = 'L';
 	
 	return 0;
 }
