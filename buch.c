fe4f5ddbe1656573fa7265eb2f7a54f7922dd014
SihdiST <sihdi.st@gmail.com>
2018-09-24 18:16:37 +0000

    Update buch.c

--- buch.c
+++ buch.c
@@ -2,17 +2,22 @@
 #include<string.h>
 #include "buch.h"
 
-void editBook(Buch* pBook, char[] pISBN, char[] pName, char[] pAuthor){
+void editBook(Buch* pBook, char pISBN[], char pName[], char pAuthor[]){
 	Book edit = createBook(pISBN, pName, pAuthor);
 	*pBook = edit;
 }
 
-Buch createBook(char[] pISBN, char[] pName, char[] pAuthor){
+Buch createBook(char pISBN[], char pName[], char pAuthor[]){
 	Buch new;
 	strcpy(new.isbn, pISBN);
 	strcpy(new.name, pName);
 	strcpy(new.author, pAuthor);
 	return new;
+}
+
+int cmpBooks(Buch pCmp1, Buch pCmp2){
+	if((strcmp(pCmp1.isbn, pCmp2.isbn) == 0) && (strcmp(pCmp1.name, pCmp2.name) == 0) && (strcmp(pCmp1.author, pCmp2.author) == 0)) return 0;
+	else return 1; 
 }
 
 void print(Buch){
